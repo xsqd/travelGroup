ca5fe75f38e7c9e64c3aa57e82add715bf8a3831
hzr-8 <1149025485@qq.com>
2019-11-30 16:36:18 +0800

    01-项目初始化，删除了不要的文件，修改了配置文件，初始化默认布局

---
+++ pages/air/index.vue
@@ -0,0 +1,14 @@
+<template>
+ <div>机票首页</div>
+</template>
+
+<script>
+
+export default {
+
+}
+</script>
+
+<style>
+
+</style>
---
+++ pages/index.vue
@@ -0,0 +1,14 @@
+<template>
+ <div>闲云首页</div>
+</template>
+
+<script>
+
+export default {
+
+}
+</script>
+
+<style>
+
+</style>
