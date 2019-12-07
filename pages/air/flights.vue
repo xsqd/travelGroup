8a8ac1f234e6ec4d196606c5b688d0816059642c
hzr-8 <1149025485@qq.com>
2019-12-07 18:56:32 +0800

    封装 mounted  单独更新数据

--- pages/air/flights.vue
+++ pages/air/flights.vue
@@ -4,7 +4,7 @@
       <!-- 顶部过滤列表 -->
       <div class="flights-content">
         <!-- 过滤条件 -->
-        <FlightsFilters :flightsData="cacheFlightsData" @setFlightsData="setFlightsData"/>
+        <FlightsFilters :flightsData="cacheFlightsData" @setFlightsData="setFlightsData" />
 
         <!-- 航班头部布局 -->
         <div>
@@ -21,25 +21,24 @@
           <!-- current-page: 当前页数 -->
           <!-- page-size：当前条数 -->
           <!-- total：总条数 -->
-          <el-pagination 
-            
+          <el-pagination
             layout="prev, pager, next, jumper, sizes, total"
-           :total="flightsData.flights.length"
-           :page-sizes="[5, 10, 15, 20]"
-           :current-page="pageIndex"
-           :page-size="pageSize"
-           @size-change="sizeChange"
+            :total="flightsData.flights.length"
+            :page-sizes="[5, 10, 15, 20]"
+            :current-page="pageIndex"
+            :page-size="pageSize"
+            @size-change="sizeChange"
             @current-change="changePageIndex"
             v-if="dataList.length > 0"
-           ></el-pagination>
-           <div v-else-if="!loading">本页暂无数据</div>
+          ></el-pagination>
+          <div v-else-if="!loading">本页暂无数据</div>
         </div>
       </div>
 
       <!-- 侧边栏 -->
       <div class="aside">
         <!-- 侧边栏组件 -->
-        <FlightsAside/>
+        <FlightsAside />
       </div>
     </el-row>
   </section>
@@ -49,8 +48,8 @@
 import moment from "moment";
 import FlightsListHead from "@/components/air/flightsListHead.vue";
 import FlightsItem from "@/components/air/flightsItem.vue";
-import FlightsFilters from "@/components/air/flightsFilters.vue"
-import FlightsAside from "@/components/air/flightsAside.vue"
+import FlightsFilters from "@/components/air/flightsFilters.vue";
+import FlightsAside from "@/components/air/flightsAside.vue";
 
 export default {
   components: {
@@ -61,7 +60,7 @@ export default {
   },
   data() {
     return {
-      loading:true,
+      loading: true,
       cacheFlightsData: {
         flights: [],
         info: {},
@@ -74,68 +73,62 @@ export default {
       }, // 航班总数据
       // dataList 可以放在计算属性里面,无需每次都调用函数进行计算
       // dataList: [], // 航班列表数据，用于循环flightsItem组件，单独出来是因为要分页
-      pageIndex:1,//当前页数
-      pageSize:5//显示条数
+      pageIndex: 1, //当前页数
+      pageSize: 5 //显示条数
     };
   },
   computed: {
-    dataList(){
-      var start = (this.pageIndex-1)*this.pageSize
-      var end = start + this.pageSize
+    dataList() {
+      var start = (this.pageIndex - 1) * this.pageSize;
+      var end = start + this.pageSize;
       //数组slice方法接受两个参数，第一个是切割的开始（包括当前index），第二个是切割的结束（不g包括当前index）
-      return this.flightsData.flights.slice(start,end)
+      return this.flightsData.flights.slice(start, end);
     }
   },
   methods: {
-    changePageIndex(pageIndex){
-      this.pageIndex = pageIndex
+    changePageIndex(pageIndex) {
+      this.pageIndex = pageIndex;
     },
-    sizeChange(pageSize){
-      this.pageSize = pageSize
+    sizeChange(pageSize) {
+      this.pageSize = pageSize;
     },
-    setFlightsData(newFlightsList){
+    setFlightsData(newFlightsList) {
       console.log("修改机票筛选参数被触发");
       //接受到新的机票数据
-      this.flightsData.flights = newFlightsList
+      this.flightsData.flights = newFlightsList;
+    },
+    loadPageData() {
+      this.$axios({
+        url: "/airs",
+        //参数可以通过 this.$route.query
+        // 这里面数据本来就是一个对象,所以无需自己拼接
+        params: this.$route.query
+      }).then(res => {
+        console.log(res);
+        this.flightsData = res.data;
+        // 这里是分页, 我们需要拿到数据的开始index 和结尾的 index
+        // this.dataList = this.flightsData.flights;
+        // 为了避免引用类型数据污染问题,需要进行深拷贝
+        this.cacheFlightsData = { ...this.flightsData };
+
+        this.loading = false;
+      });
     }
   },
   mounted() {
     // console.log(this.$route.query);
     // 对于路由,两种参数分别是 params 和 query
     // 对于 axios 两种参数分别是 data 和 params
-    this.$axios({
-      url: "/airs",
-      //参数可以通过 this.$route.query
-      // 这里面数据本来就是一个对象,所以无需自己拼接
-      params: this.$route.query
-    }).then(res => {
-      console.log(res);
-      this.flightsData = res.data;
-      // 这里是分页, 我们需要拿到数据的开始index 和结尾的 index
-      // this.dataList = this.flightsData.flights;
-      // 为了避免引用类型数据污染问题,需要进行深拷贝
-      this.cacheFlightsData = { ...this.flightsData };
-      
-      this.loading = false
-    });
+    this.loadPageData();
   },
   watch: {
-    $route(){
-      this.$axios({
-      url: "/airs",
-      //参数可以通过 this.$route.query
-      // 这里面数据本来就是一个对象,所以无需自己拼接
-      params: this.$route.query
-    }).then(res => {
-      console.log(res);
-      this.flightsData = res.data;
-      // 这里是分页, 我们需要拿到数据的开始index 和结尾的 index
-      // this.dataList = this.flightsData.flights;
-      // 为了避免引用类型数据污染问题,需要进行深拷贝
-      this.cacheFlightsData = { ...this.flightsData };
-      
-      this.loading = false
-    });
+    $route() {
+      console.log(this.$route.query.departCity);
+      // 这里一旦触发证明用户点击了历史记录
+      // location.reload();
+      // 这种强制刷新,迫不得已的方法
+      // 只需要更新数据即可
+      this.loadPageData();
     }
   }
 };
