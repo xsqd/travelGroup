7d7975cf2e33534759f1f3c93b95db6be89113f7
hzr-8 <1149025485@qq.com>
2019-12-10 10:49:38 +0800

    解决定时器bug

--- pages/air/pay.vue
+++ pages/air/pay.vue
@@ -28,7 +28,8 @@ import Qrcode from "qrcode";
 export default {
   data(){
     return{
-      orderData:{}
+      orderData:{},
+      timed:null
     }
   },
   mounted() {
@@ -45,7 +46,9 @@ export default {
   },
   destroyed() {
     // 这里是组件被销毁的声明周期钩子函数
-    // 如果你用了 setInterval 记得在这里清理定时器
+    //记得在这里清理定时器
+    console.log("支付页面跳出，应该清除定时器");
+    clearTimeout(this.timed)
   },
   methods: {
     checkPayStatus(){
@@ -65,7 +68,7 @@ export default {
         console.log(res.data);
         if(res.data.trade_state=="NOTPAY"){
           //等待支付，不断轮询
-          setTimeout(()=>{
+          this.timed = setTimeout(()=>{
             this.checkPayStatus()
           },3000)
         }else{
