b1ec18fad639256b7a938ca56f7d42c875135f65
xiegege <1522550956@qq.com>
2019-12-10 20:01:00 +0800

    创建组件编写数据

---
+++ components/post/postSearch.vue
@@ -0,0 +1,84 @@
+<template>
+  <div>asw</div>
+</template>
+
+<script>
+export default {
+  data () {
+    return {
+      posthotel: [
+        { name: '热门酒店',
+          children: [{
+            name: '北京',
+            title: '世界著名古都和现代化国际城市'
+          },
+          {
+            name: '广州',
+            title: '世界著名古都和现代化国际城市'
+          },
+          {
+            name: '上海',
+            title: '世界著名古都和现代化国际城市'
+          },
+          {
+            name: '成都',
+            title: '世界著名古都和现代化国际城市'
+          },
+          {
+            name: '西安',
+            title: '世界著名古都和现代化国际城市'
+          }]
+        },
+        { name: '推荐城市',
+          children: [{
+            name: '北京',
+            title: '世界著名古都和现代化国际城市'
+          },
+          {
+            name: '广州',
+            title: '世界著名古都和现代化国际城市'
+          },
+          {
+            name: '上海',
+            title: '世界著名古都和现代化国际城市'
+          },
+          {
+            name: '成都',
+            title: '世界著名古都和现代化国际城市'
+          },
+          {
+            name: '西安',
+            title: '世界著名古都和现代化国际城市'
+          }]
+        },
+        { name: '奔向海岛',
+          children: [{
+            name: '北京',
+            title: '世界著名古都和现代化国际城市'
+          },
+          {
+            name: '广州',
+            title: '世界著名古都和现代化国际城市'
+          },
+          {
+            name: '上海',
+            title: '世界著名古都和现代化国际城市'
+          },
+          {
+            name: '成都',
+            title: '世界著名古都和现代化国际城市'
+          },
+          {
+            name: '西安',
+            title: '世界著名古都和现代化国际城市'
+          }]
+        }
+      ]
+    }
+  }
+}
+</script>
+
+<style>
+
+</style>
