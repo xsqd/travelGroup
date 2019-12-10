3d82d0ca6f080ec92adb0397b25e27dac3fca364
hzr-8 <1149025485@qq.com>
2019-12-10 20:15:38 +0800

    创建写游记页面

---
+++ pages/post/write.vue
@@ -0,0 +1,26 @@
+<template>
+  <div class="container">
+    <el-row type="flex" justify="space-between">
+      <div class="main">
+        写游记
+      </div>
+      <div class="aside">
+        草稿箱
+      </div>
+    </el-row>
+  </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style lang="less" scoped>
+.container{
+  width: 1000px;
+  margin: 0 auto;
+  padding: 20px 0
+}
+</style>
