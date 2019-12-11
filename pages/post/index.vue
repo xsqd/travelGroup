5ae3be824b5fc444d9c45e08c69724fd01df62f7
xiegege <1522550956@qq.com>
2019-12-11 10:41:53 +0800

    左侧推荐城市img模块样式完成

--- pages/post/index.vue
+++ pages/post/index.vue
@@ -6,19 +6,21 @@
     <PostAside />
     <!-- 推荐文章列表 -->
     <PostList />
-    
+    <PostTJimg />
   </div>
 </template>
 
 <script>
-import PostList from "@/components/post/postList";
-import PostAside from "@/components/post/postAside";
+import PostList from '@/components/post/postList'
+import PostAside from '@/components/post/postAside'
+import PostTJimg from '@/components/post/postTJimg'
 export default {
   components: {
     PostList,
-    PostAside
+    PostAside,
+    PostTJimg
   }
-};
+}
 </script>
 
 <style>
