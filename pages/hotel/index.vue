9f6ba9ac77a5b866cebdb2bf50bbc0e526260c74
DX-Kirito <1148323582@qq.com>
2019-12-11 17:18:29 +0800

    酒店下方静态基本写完

--- pages/hotel/index.vue
+++ pages/hotel/index.vue
@@ -1,14 +1,22 @@
 <template>
- <div>酒店首页</div>
+ <div class="hotel">酒店首页
+    <HotelFilters />
+ </div>
 </template>
 
 <script>
-
+import HotelFilters from '@/components/hotel/hotelFilters.vue'
 export default {
-
+    components:{
+        HotelFilters
+    }
 }
 </script>
 
-<style>
-
+<style lang="less" scoped>
+.hotel {
+  width: 1000px;
+  margin: 0 auto;
+  padding: 5px 20px;
+}
 </style>
