95599566f28862cf97de7cc77d555332ddeb4150
hzr-8 <1149025485@qq.com>
2019-12-11 09:39:46 +0800

    下载并引入富文本框

--- pages/post/write.vue
+++ pages/post/write.vue
@@ -4,11 +4,13 @@
       <div class="main">
         <h2>发表新攻略</h2>
         <p>分享你的个人游记，让更多人看到哦！</p>
-        <el-form ref="form" :model="form" label-width="80px">
-          <el-form-item label-width="0px">
-            <el-input v-model="input" placeholder="请输入标题" />
+        <el-form ref="form" :model="form">
+          <el-form-item>
+            <el-input placeholder="请输入标题" />
           </el-form-item>
-          <div>富文本框</div>
+          <el-form-item>
+            <VueEditor ref="vueEditor" :config="config" style="height:458px;margin-bottom:50px" />
+          </el-form-item>
           <el-form-item label="选择城市">
             <el-input placeholder="请搜索游玩城市" style="width:200px" />
           </el-form-item>
@@ -36,8 +38,45 @@
 </template>
 
 <script>
-export default {
+import 'quill/dist/quill.snow.css'
+let VueEditor
 
+if (process.browser) {
+  VueEditor = require('vue-word-editor').default
+}
+export default {
+  components: {
+    VueEditor
+  },
+  data () {
+    return {
+      form: {
+        input: ''
+      },
+      config: {
+        // 上传图片的配置
+        uploadImage: {
+          // 处理文件上传的服务器地址
+          url: 'http://localhost:3000/upload',
+          // 后台需要的参数名称
+          name: 'file',
+          // res是结果，insert方法会把内容注入到编辑器中，res.data.url是资源地址
+          uploadSuccess (res, insert) {
+            insert('http://localhost:3000' + res.data.url)
+          }
+        },
+
+        // 上传视频的配置
+        uploadVideo: {
+          url: 'http://localhost:3000/upload',
+          name: 'file',
+          uploadSuccess (res, insert) {
+            insert('http://localhost:3000' + res.data.url)
+          }
+        }
+      }
+    }
+  }
 }
 </script>
 
