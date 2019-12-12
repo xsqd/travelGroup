c2b93911c8dba7c23211f2045c3a217dd5a5b09b
hzr-8 <1149025485@qq.com>
2019-12-12 15:47:43 +0800

    修改写游记按钮bug

--- pages/post/index.vue
+++ pages/post/index.vue
@@ -17,9 +17,9 @@
             <h4>推荐攻略</h4>
           </el-col>
           <el-col :span="4" :offset="16" class="btn">
-            <el-button type="primary">
+            <el-button @click="$router.push({path: '/post/write'})" type="primary">
               <i class="el-icon-edit" />
-              <span @click="$router.push({path: '/post/write'})">写游记</span>
+              <span>写游记</span>
             </el-button>
           </el-col>
         </el-row>
@@ -32,76 +32,76 @@
           :current-page="currentPage"
           :page-sizes="[3, 6, 9, 12]"
           :page-size="3"
-          layout="total, sizes, prev, pager, next, jumper"
           :total="postList.length"
-        ></el-pagination>
+          layout="total, sizes, prev, pager, next, jumper"
+        />
       </el-col>
     </el-row>
   </div>
 </template>
 
 <script>
-import PostList from "@/components/post/postList";
-import PostAside from "@/components/post/postAside";
-import PostTJimg from "@/components/post/postTJimg";
-import PostSearch from "@/components/post/postSearch";
+import PostList from '@/components/post/postList'
+import PostAside from '@/components/post/postAside'
+import PostTJimg from '@/components/post/postTJimg'
+import PostSearch from '@/components/post/postSearch'
 export default {
-  data() {
-    return {
-      postList: [], // 渲染列表的数据
-      currentPage: 1, // 当前页
-      currentPageSize: 3, // 当前每页显示条数
-      postLists: []
-    };
-  },
   components: {
     PostList,
     PostAside,
     PostTJimg,
     PostSearch
   },
-  computed: {
-    // 计算一个变量，这个变量是用来进行分页的
-    dataList() {
-      // 列表开始的索引
-      var start = (this.currentPage - 1) * this.currentPageSize;
-      // 列表结束的索引
-      var end = start + this.currentPageSize;
-      return this.postList.slice(start, end);
+  data () {
+    return {
+      postList: [], // 渲染列表的数据
+      currentPage: 1, // 当前页
+      currentPageSize: 3, // 当前每页显示条数
+      postLists: []
     }
   },
-  mounted() {
-    this.loadPage();
+  computed: {
+    // 计算一个变量，这个变量是用来进行分页的
+    dataList () {
+      // 列表开始的索引
+      const start = (this.currentPage - 1) * this.currentPageSize
+      // 列表结束的索引
+      let end = start + this.currentPageSize
+      return this.postList.slice(start, end)
+    }
+  },
+  mounted () {
+    this.loadPage()
   },
   methods: {
     // 请求数据加载封装
-    loadPage() {
+    loadPage () {
       // 获取列表数据
       this.$axios({
-        url: "/posts"
-      }).then(res => {
+        url: '/posts'
+      }).then((res) => {
         // console.log(res);
-        this.postList = res.data.data;
-        this.postLists = [...res.data.data];
-        console.log(this.postList);
-      });
+        this.postList = res.data.data
+        this.postLists = [...res.data.data]
+        console.log(this.postList)
+      })
     },
     // 每页显示的文章条数
-    handleSizeChange(currentPageSize) {
-      console.log(currentPageSize);
-      this.currentPageSize = currentPageSize;
+    handleSizeChange (currentPageSize) {
+      console.log(currentPageSize)
+      this.currentPageSize = currentPageSize
     },
     // 当前页面是第几页
-    handleCurrentChange(currentPage) {
-      console.log(currentPage);
-      this.currentPage = currentPage;
+    handleCurrentChange (currentPage) {
+      console.log(currentPage)
+      this.currentPage = currentPage
     },
     // 筛选后重新渲染页面文章数据
-    postlistAsides(item) {
-      this.postList = item;
+    postlistAsides (item) {
+      this.postList = item
     }
   }
-};
+}
 </script>
 
 <style lang="less" scoped>
